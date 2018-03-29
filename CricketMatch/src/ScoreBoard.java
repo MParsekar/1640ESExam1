5dd2e188bbb8215d9df04ad4de4de3824fc62806
MParsekar <parsekarmahadev@gmail.com>
2018-03-29 15:29:48 +0530

    Final Class NOt Tested

--- CricketMatch/src/ScoreBoard.java
+++ CricketMatch/src/ScoreBoard.java
@@ -1,22 +1,43 @@
 
 public class ScoreBoard {
 	private int totalOver=0;
-	private Over over[]=new Over[50];
+	private Over[] over;
 	private int totalBalls;
 	
+	
+	public ScoreBoard() {
+		super();
+		
+		over=new Over[10];
+		over[0]=new Over();
+	}
 	public void addBalls(int runs){
+		
 		over[totalOver].addRunsperBall(runs);
 		totalBalls++;
 		if(totalBalls%6==0){
 			totalOver++;
+			over[totalOver]=new Over();
 		}
 	}
+	
+	
+
 	public int getCurrentScore(){
 		int currentScore=0;
-		for(int i=0;i<6;i++){
+		for(int i=0;i<totalOver;i++){
 			currentScore+=over[i].getScorePerOver();
 		}
 		return currentScore;
+	}
+	public void printScoreCard(){
+		System.out.println("CurrentScore: "+ this.getCurrentScore());
+	}
+	public void getScorePer(){
+		for(int i=0;i<totalOver;i++){
+			System.out.println("Over 1: "+(i+1));
+			over[i].printScorePerBallInCurrentOver();
+		}
 	}
 }	
 

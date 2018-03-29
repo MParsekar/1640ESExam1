f0e75eed57d8321c026339b8eb07f6d7bf17ab80
MParsekar <parsekarmahadev@gmail.com>
2018-03-29 15:29:32 +0530

    Final Class Not Tested

--- CricketMatch/src/Over.java
+++ CricketMatch/src/Over.java
@@ -3,6 +3,7 @@ public class Over {
 	private int overBalls[]=new int[6];
 	private int totalRunsOver=0;
 	int currentBallofTheOver=0;
+	
 	public void addRunsperBall(int runs){
 		overBalls[currentBallofTheOver]=runs;
 		currentBallofTheOver++;
@@ -13,7 +14,7 @@ public class Over {
 	}
 	public void printScorePerBallInCurrentOver(){
 		for(int i=0; i<currentBallofTheOver;i++){
-			System.out.println("Ball "+i+" : "+overBalls[i]);
+			System.out.println("\tBall "+i+" : "+overBalls[i]);
 		}
 	}
 }
